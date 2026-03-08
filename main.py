13a2482de277a86c3acb6d8825e98a879cba8e0d
emilyng-sz <emilyng.sz831@gmail.com>
2026-03-08 18:12:40 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from dotenv import load_dotenv
+
+from extract_load.logger import setup_logger
+from extract_load.raw_table_schemas import RAW_TABLE_SCHEMAS
+from extract_load.ingestion_raw import load_tables
+
+logger = setup_logger("logs/pipeline.log")
+load_dotenv("config.env")
+
+load_tables(
+    check_exists = False
+)
