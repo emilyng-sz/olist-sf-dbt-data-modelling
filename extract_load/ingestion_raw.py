c05d33a8102be3be277fa20a8d41c1797805ff0f
emilyng-sz <emilyng.sz831@gmail.com>
2026-03-09 00:03:30 +0800

    Update ingestion_raw.py with MFA error handling

--- extract_load/ingestion_raw.py
+++ extract_load/ingestion_raw.py
@@ -3,22 +3,39 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-from logger import setup_logger
+from extract_load.logger import setup_logger
 from extract_load.raw_table_schemas import RAW_TABLE_SCHEMAS
 
 logger = setup_logger("logs/pipeline.log")
 load_dotenv("config.env")
 
-def load_tables():
+def load_tables(
+        full_reload: bool = True,
+        check_exists: bool = True
+):
     """
     Main function to load all tables to Snowflake from RAW_DATA_DIR
+    full_reload: True if overwrite, False if append
+    check_exists: SQL PUT and COPY INTO will not run if table already exists
     """
+    logger.info("Loading tables with full_reload {} and check_exists {}".format(
+        full_reload, check_exists
+    ))
     cursor_raw = _init_snowflake_conn(
         schema="raw"
     )
+    # Check if cursor is valid
+    if not cursor_raw:
+        logger.error(f"Function load_tables() existed due to invalid snowflake cursor")
+        return
+    
     for file in Path(os.getenv("RAW_DATA_DIR")).iterdir():
         if file.is_file():
-            _upload_csv_to_raw(cursor=cursor_raw, file_path=file)
+            _upload_csv_to_raw(
+                cursor=cursor_raw,
+                file_path=file,
+                full_reload=full_reload,
+                check_exists=check_exists)
 
 def _init_snowflake_conn(
         schema: str = None) -> snowflake.connector.cursor.SnowflakeCursor:
@@ -53,21 +70,29 @@ def _init_snowflake_conn(
     
     except Exception as e:
         logger.error(f"Error enountered in _init_snowflake_conn: {e}")
+        if "Multi-factor authentication is required" in str(e):
+            logger.warning("May have to run bypass MFA for database. "
+                           "Consider: `ALTER USER olist_svc SET MINS_TO_BYPASS_MFA = 0`")
+        return
 
 def _upload_csv_to_raw(
         cursor: snowflake.connector.cursor.SnowflakeCursor,
-        file_path: Path
+        file_path: Path,
+        full_reload: bool = True,
+        check_exists: bool = True,
 ) -> None:
     """
     Uploads a local file to raw schema on snowflake
     """
+    # Check if cursor is valid
+    if not cursor:
+        return ConnectionError("Snowflake connector failed to initialise")
+    
     # Define variables
     table_name = file_path.stem
     stage_name = os.getenv("SF_RAW_STAGE_NAME")
     ddl = RAW_TABLE_SCHEMAS.get(file_path.name, None)
     abs_file_path = os.getcwd() / file_path
-
-    logger.info(f"Stage and copy file: {file_path} as table: {table_name}")
 
     # 1. Create Table if not exists
     logger.info(f"[1/3] Ensuring table {table_name} exists...")
@@ -77,9 +102,12 @@ def _upload_csv_to_raw(
     ddl_result = cursor.execute(ddl).fetchone()
     logger.debug(f"      Status: {ddl_result[0]}")
 
-    if "already exists, statement succeeded." in ddl_result[0]:
+    if check_exists and "already exists, statement succeeded." in ddl_result[0]:
         logger.info("Early return since table exists")
         return
+    
+    elif not ("already exists, statement succeeded." in ddl_result[0]):
+        logger.debug("Table does not exist. Creating table")
 
     # 2. Stage the file into Snowflake's internal stage
     logger.info(f"[2/3] Staging file {abs_file_path}")
@@ -91,6 +119,9 @@ def _upload_csv_to_raw(
 
     # 3. COPY INTO the target table by bulk loading
     logger.info(f"[3/3] Loading into OLIST_DB.RAW.{table_name}")
+    if full_reload:
+        logger.info(f"First truncating table: {table_name}")
+        cursor.execute(f"TRUNCATE TABLE {table_name}")
     copy_result = cursor.execute(f"""
         COPY INTO OLIST_DB.RAW.{table_name}
         FROM @{stage_name}/{file_path.name}.gz
