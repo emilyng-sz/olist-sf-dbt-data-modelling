dd2264ea911c9f8a79bf8fe054b0055d8f07a99d
emilyng-sz <emilyng.sz831@gmail.com>
2026-03-23 21:54:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from pathlib import Path
-from dotenv import load_dotenv
-
-from extract_load.logger import setup_logger
-from extract_load.raw_table_schemas import RAW_TABLE_SCHEMAS
 from extract_load.ingestion_raw import load_tables
-
-#logger = setup_logger("logs/pipeline.log")
-#load_dotenv("config.env")
 
 load_tables(
     full_reload=True,
-    check_exists=True
+    reload_schema=True,
+    check_exists=False
 )
